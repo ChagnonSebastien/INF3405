453a576364a84aae8c0b8c693ce7ea01e773e4da
Sebastien <chagnon.s21@gmail.com>
2018-05-10 10:11:03 -0400

    Initial commit and project initialisation

---
+++ client/src/ca/polymtl/inf3405/tp1/client/Main.java
@@ -0,0 +1,9 @@
+package ca.polymtl.inf3405.tp1.client;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ server/src/ca/polymtl/inf3405/tp1/server/Main.java
@@ -0,0 +1,10 @@
+package ca.polymtl.inf3405.tp1.server;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
